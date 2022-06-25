7e11ea11b154086d2d0f1955c2338636d2dfc26b
phxswjjj <phxswjjj@gmail.com>
2022-06-25 21:05:30 +0800

    add swagger config

--- MySwagger/App_Start/WebApiConfig.cs
+++ MySwagger/App_Start/WebApiConfig.cs
@@ -13,12 +13,6 @@ namespace MySwagger
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
--- MySwagger/Global.asax.cs
+++ MySwagger/Global.asax.cs
@@ -1,3 +1,5 @@
+using NSwag.AspNet.Owin;
+using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +9,28 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(MySwagger.WebApiApplication))]
+
 namespace MySwagger
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapOwinPath("swagger", app =>
+            {
+                app.UseSwaggerUi3(typeof(WebApiApplication).Assembly, settings =>
+                {
+                    settings.MiddlewareBasePath = "/swagger";
+
+                    settings.PostProcess = document =>
+                    {
+                        document.Info.Title = "WebAPI NSwag";
+                    };
+                });
+            });
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
