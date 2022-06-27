2c96cefab1f492ed7d5d96a24820a6fc7e7584bb
phxswjjj <phxswjjj@gmail.com>
2022-06-27 23:21:09 +0800

    禁止重複執行

---
+++ MySwagger/Controllers/DispController.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MySwagger.Controllers
+{
+    [RoutePrefix("api/Disp")]
+    public class DispController : ApiController
+    {
+        static readonly TimeSpan Expired = new TimeSpan(0, 0, 3);
+
+        [Route("MoveIn")]
+        [HttpGet]
+        public IHttpActionResult MoveIn(string lotId)
+        {
+            var redis = ConnectionMultiplexer.Connect("redis.local:6379");
+            var db = redis.GetDatabase();
+            var isSuccess = db.StringSet($"DISP:LOT:{lotId}", Environment.MachineName, 
+                expiry: Expired, when: When.NotExists);
+            if (isSuccess)
+                return Ok();
+            else
+                return InternalServerError();
+        }
+    }
+}
